8839eacee4a7254df0b56f9b2dd24da1d2a8a80b
Sidney MS Robinson Jr <51104100+Coolbust@users.noreply.github.com>
2020-01-06 16:00:49 -0800

    Create Metronome.py

---
+++ Metronome.py
@@ -0,0 +1,15 @@
+import time
+import winsound
+def main(bpm = 120, bpb = 4):
+        sleep = 60.0 / bpm
+        counter = 0
+        while True:
+            counter += 1
+            if counter % bpb:
+                print('tick')
+                winsound.PlaySound("*",winsound.SND_ASYNC)
+            else:
+                print('TICK')
+            time.sleep(sleep)
+
+main()
